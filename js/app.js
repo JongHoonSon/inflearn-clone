0b50c408daeb1bdb3972bc47677ffd9765c3d5b6
JongHoonSon <roka1684@gmail.com>
2022-10-11 22:54:40 +0900

    Implement sticky on header

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 const header = document.getElementById("header");
+const main = document.getElementById("main");
 
 window.addEventListener("scroll", function () {
   const pos = window.scrollY;
 
   if (pos > 103) {
     header.classList.add("sticky");
+    main.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
+    main.classList.remove("sticky");
   }
 });
